48fb718de8cf04b419fddb02ffefe882351842c2
Zach YU <git@yuz.me>
2014-04-24 01:11:12 +0200

    fix off canvas bug and many updates

--- js/main.js
+++ js/main.js
@@ -37,8 +37,8 @@
     offset: 0
   });
 
-  $("[data-toggle=offcanvas]").click(function() {
-    $(".row-offcanvas").toggleClass("active");
+  $(".menu-toggle").click(function() {
+    $(".off-canvas").toggleClass("active");
   });
 
 }).call(this);
